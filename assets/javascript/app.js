924e6147e977fee3bbafe8698ea8cf52fbee477a
Brad Nolan <bnolan56@gmail.com>
2018-06-23 21:08:48 -0600

    formatting hidden classes adding winchecks and more functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,45 +8,45 @@ $(document).ready(function() {
 
   // global variables
   var questionIndex = 0;
-  var incorrectAns = 0;
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
   var notAnswered = 0;
-  var currentQuestion = 0;
 
   // question array
   var charDee = [{
     question: "What is Rickety Cricket's real name?",
     possibleAnswers: ["Former Father Matthew Mara", "Psycho Pete", "Lion McPoyle", "Reggie"],
     correct: 0,
-    correctScreen: "Rickety Rick rick Crickettt. Matty Mara was his name",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! The answer was Former Father Matthew Mara",
+    correctChoice: "Rickety Rick rick Crickettt. Matty Mara was his name",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was Former Father Matthew Mara",
   },
   {
     question: 'What is the best food to make you go to sleep?',
     possibleAnswers: ["Beer", "Coon Meat", "Cat Food", "Wine in a Can"],
     correct: 2,
-    correctScreen: "Yup you eat cat food and huff some glue, your stomach gurgles and it drowns the cat cries at night out.",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! The answer was Cat Food",
+    correctChoice: "Yup you eat cat food and huff some glue, your stomach gurgles and it drowns the cat cries at night out.",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was Cat Food",
   },
   {
     question: 'Who has Charlie and Frank been hanging with, under a bridge?',
     possibleAnswers: ["Z", "Bruce Mathis", "Old Black Man", "Duncan"],
     correct: 3,
-    correctScreen: "Yeah Frank? I know that bitch.",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! The answer was Duncan, Frank's bridge friend",
+    correctChoice: "Yeah Frank? I know that bitch.",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was Duncan, Frank's bridge friend",
   },
   {
     question: "How famous is Mac's famous Mac & Cheese?",
     possibleAnswers: ["If it's so famous, why haven't I heard of it?", "Mildly Famous", "Gross, it's made with dog meat", "It's just Kraft Mac & Cheese"],
     correct: 0,
-    correctScreen: "Mac's famous mac and cheese? I'm your best friend, and, I've never heard of it",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! The answer was if it's so famous, why haven't I heard of it",
+    correctChoice: "Mac's famous mac and cheese? I'm your best friend, and, I've never heard of it",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was if it's so famous, why haven't I heard of it",
   },
   {
     question: "Who is the Nightman's archenemy?",
     possibleAnswers: ['The Lawyer', 'Uncle Jack', 'Dayman: Champion of the Sun', 'Carol'],
     correct: 2,
-    correctScreen: "Dayman! AHHH AHHHHHH Champion of the Sun",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! The answer was Dayman Champion of the Sun",
+    correctChoice: "Dayman! AHHH AHHHHHH Champion of the Sun",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was Dayman Champion of the Sun",
   },
   {
     question: 'What is the D.E.N.N.I.S System?',
@@ -54,50 +54,104 @@ $(document).ready(function() {
     'Demonstrate looks, Encompass sexually, Neglect physically , Nuture hope, Inspire dependence, Smoothly exit','Dennis is a Name',
     'Degrade looks, Engage emotionally, Nurture dependence, Nevermind opinions, Intimidate personally, Show worth'],
     correct: 0,
-    correctScreen: "DENNIS SYSTEM, the true method to winning a girl's heart",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! Demonstrate value, Engage physically, Nurture dependence, Neglect emotionally, Inspire hope, Separate entirely",
+    correctChoice: "DENNIS SYSTEM, the true method to winning a girl's heart",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! Demonstrate value, Engage physically, Nurture dependence, Neglect emotionally, Inspire hope, Separate entirely",
   },
   {
     question: "What is Nightcrawlers?",
     possibleAnswers: ['A worm for fishing', 'A game where the lights are turned off and you wiggle on the floor like a worm', 'You link arms and pretend to be a worm on a hook', 'A stupid catch phrase'],
     correct: 1,
-    correctScreen: "Nightcrawlers! I love that game, we can play if you want",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! The answer was a game where you turn the lights off and wiggle on the floor like a worm",
+    correctChoice: "Nightcrawlers! I love that game, we can play if you want",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was a game where you turn the lights off and wiggle on the floor like a worm",
   },
   {
     question: "What level did Duncan get to in his game?",
     possibleAnswers: ['LEVEL TWOO', 'Z and Duncan were DJing', 'LEVEL 3 SON! LEVEL 3!', 'SHABOOYA!'],
     correct: 2,
-    correctScreen: "Oh shit son! Level 3!",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! The answer was LEVEL 3 SON! LEVEL 3!",
+    correctChoice: "Oh shit son! Level 3!",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was LEVEL 3 SON! LEVEL 3!",
   },
   {
     question: "Dennis is Asshole, Why Charlie Hate?",
     possibleAnswers: ['DENNIS IS ASSHOLE', 'The Nightman Cometh', "Dennis doesn't know shit about girls", 'BECAUSE DENNIS IS A BASTARD MAN'],
     correct: 3,
-    correctScreen: "Yeah Frank? I know that bitch.",
-    incorrectScreen: "SHABOOYA! YOU'RE WRONG! The answer was BECAUSE DENNIS IS A BASTARD MAN",
+    correctChoice: "Yeah Frank? I know that bitch.",
+    incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was BECAUSE DENNIS IS A BASTARD MAN",
   },
 ];
 
 //// FUNCTIONS ////
 
-  //function that displays current question to be answered
+
+  //unhides scoreboard initially, but also used as sort of a "refresh" function.
+  function showScoreboard() {
+    $('.scoreboard').removeClass("hidden");
+    $('#rightanswers').html("Correct answers: " + correctAnswers);
+    $('#wronganswers').html("Incorrect answers: " + incorrectAnswers);
+  }
+
+  // function that displays current question to be answered
   function showQuestion(questionIndex) {
     $("#question").removeClass("hidden");
     $("#question").text(charDee[questionIndex].question);
     callTimer();
   }
 
+  // function that shows possible answer buttons as choices
   function showPossibleAnswers(questionIndex) {
-    for (var i = 0; i < charDee[questionIndex].length; i++) {
+    for (var i = 0; i < charDee[questionIndex].possibleAnswers.length; i++) {
       var button = $("<button>");
       button.addClass("btn");
       button.addClass("btn-default");
+      button.addClass("btn-choice");
       button.attr("data-index", i);
-      button.text(charDee[questionIndex].possibleAnswers);
+      button.text(charDee[questionIndex].possibleAnswers[i]);
       $("#options").append(button);
     }
+  }
+
+  // function that shows teh correct choice
+  function correctChoiceShown() {
+    $('#results').html(charDee[questionIndex].correctChoice);
+  }
+
+  // function that shows the wrong choice
+  function incorrectChoiceShown() {
+    $('#results').html(charDee[questionIndex].incorrectChoice);
+  }
+
+  function winCheck() {
+    var scoreCalc = Math.floor(correctAnswers / (correctAnswers + incorrectAnswers) * 100)
+    var totalScore = scoreCalc + "%";
+
+    if (scoreCalc >= 70) {
+      console.log({
+        "Total Score": totalScore
+      });
+
+      $('#choices').html("YOU WIN WITH AN AMAZING " + totalScore);
+      $('#choices').append("<button class='btn resetbutton'>Reset Game</button>")
+      $('.resetbutton').on("click", function () {
+        location.reload();
+      });
+    }
+
+    if (scoreCalc < 70) {
+      console.log({
+        "Total Score": totalScore
+      });
+
+      $('#choices').html("YOU LOSE WITH A TERRIBLE " + totalScore);
+      $('#choices').append("<button class='btn resetbutton'>Reset Game</button>")
+      $('.resetbutton').on("click", function () {
+        location.reload();
+      });
+    }
+  }
+
+  // clears previous buttons
+  function buttonClear() {
+    $("button").remove();
   }
 
   // function that shows my timer
@@ -118,16 +172,16 @@ $(document).ready(function() {
 
   // function that runs my timer, interval delay of 1 second
   function runTimer() {
-    var timerCount = 21;
+    var timerCount = 16;
     var timerInterval = setInterval( function() {
       timerCount--;
       $("#timeCounter").text(timerCount);
 
       if(timerCount === 0) {
-        incorrectAns++;
+        incorrectAnswers++;
 
         clearInterval(timerInterval);
-        timerCount = 21;
+        timerCount = 15;
 
         $("#timeCounter").text(timerCount);
 
@@ -149,10 +203,62 @@ $(document).ready(function() {
 
 //// EVENT LISTENERS ////
 
+  // start game on click event that runs first functions
   $("#start").on('click', function() {
+    $("#start").hide();
     showTimerDiv();
     showQuestion(questionIndex);
-    showPossibleAnswers();
+    showPossibleAnswers(questionIndex);
+
+  });
+
+  //button on click functions.
+  $(document).on("click", ".btn-choice", function () {
+    var answerText = $(this).text();
+    var answerDataValue = $(this).attr("data-index");
+    var answerIndex = parseInt(answerDataValue);
+
+    //control flow for answering questions
+    if (charDee[questionIndex].correct === answerIndex) {
+      $("<button>").addClass("btn-success");
+      correctChoiceShown();
+      // buttonClear();
+      correctAnswers++
+
+      if (questionIndex + 1 === charDee.length) {
+        winCheck();
+        showScoreboard()
+      }
+
+      if (questionIndex + 1 < charDee.length) {
+        setTimeout(function () {
+          buttonClear();
+          showQuestion(questionIndex);
+          showPossibleAnswers(questionIndex);
+        }, 5000);
+      }
+    }
+
+    if (charDee[questionIndex].correct !== answerIndex) {
+      incorrectChoiceShown();
+      buttonClear();
+      incorrectAnswers++
+
+      if (questionIndex + 1 === charDee.length) {
+        winCheck();
+        showScoreboard()
+      }
+
+      if (questionIndex + 1 < charDee.length) {
+        setTimeout(function () {
+          buttonClear();
+          showQuestion(questionIndex);
+          showPossibleAnswers(questionIndex);
+        }, 5000);
+      }
+    }
+
+    questionIndex++;
   });
 
 //     // FUNCTIONS
