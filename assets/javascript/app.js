9562d09560f37d61309038331ecd9da1d7ac239f
Brad Nolan <bnolan56@gmail.com>
2018-06-23 21:48:17 -0600

    fixing styling and debugging

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
     question: "Dennis is Asshole, Why Charlie Hate?",
     possibleAnswers: ['DENNIS IS ASSHOLE', 'The Nightman Cometh', "Dennis doesn't know shit about girls", 'BECAUSE DENNIS IS A BASTARD MAN'],
     correct: 3,
-    correctChoice: "Yeah Frank? I know that bitch.",
+    correctChoice: "Yup, that was totally me who wronte that. Dennis is a bastard man",
     incorrectChoice: "SHABOOYA! YOU'RE WRONG! The answer was BECAUSE DENNIS IS A BASTARD MAN",
   },
 ];
@@ -86,8 +86,9 @@ $(document).ready(function() {
   //unhides scoreboard initially, but also used as sort of a "refresh" function.
   function showScoreboard() {
     $('.scoreboard').removeClass("hidden");
-    $('#rightanswers').html("Correct answers: " + correctAnswers);
-    $('#wronganswers').html("Incorrect answers: " + incorrectAnswers);
+    $('#correct-answers').html("Correct answers: " + correctAnswers);
+    $('#incorrect-answers').html("Incorrect answers: " + incorrectAnswers);
+    $('#not-answered').html("Not answered: " + notAnswered);
   }
 
   // function that displays current question to be answered
@@ -112,12 +113,12 @@ $(document).ready(function() {
 
   // function that shows teh correct choice
   function correctChoiceShown() {
-    $('#results').html(charDee[questionIndex].correctChoice);
+    $('#results').html("<i class='fas fa-check-circle correct-choice-shown'></i> <br /> <p class='correct-choice-shown'>" + charDee[questionIndex].correctChoice + "</p>");
   }
 
   // function that shows the wrong choice
   function incorrectChoiceShown() {
-    $('#results').html(charDee[questionIndex].incorrectChoice);
+    $('#results').html("<i class='fas fa-times-circle incorrect-choice-shown'></i> <br /> <p class='incorrect-choice-shown'>" + charDee[questionIndex].incorrectChoice + "</p>");
   }
 
   function winCheck() {
@@ -129,8 +130,8 @@ $(document).ready(function() {
         "Total Score": totalScore
       });
 
-      $('#choices').html("YOU WIN WITH AN AMAZING " + totalScore);
-      $('#choices').append("<button class='btn resetbutton'>Reset Game</button>")
+      $('#results').html("<h3 class='win'>YOU WIN WITH AN AMAZING " + totalScore + "</h3>");
+      $('#choices').append("<button class='btn btn-md btn-warning resetbutton'>Reset Game</button>")
       $('.resetbutton').on("click", function () {
         location.reload();
       });
@@ -141,8 +142,8 @@ $(document).ready(function() {
         "Total Score": totalScore
       });
 
-      $('#choices').html("YOU LOSE WITH A TERRIBLE " + totalScore);
-      $('#choices').append("<button class='btn resetbutton'>Reset Game</button>")
+      $('#results').html("<h3 class='loss'>YOU LOSE WITH A TERRIBLE " + totalScore + "</h1>");
+      $('#choices').append("<button class='btn btn-md btn-warning resetbutton'>Reset Game</button>")
       $('.resetbutton').on("click", function () {
         location.reload();
       });
@@ -222,8 +223,9 @@ $(document).ready(function() {
     if (charDee[questionIndex].correct === answerIndex) {
       $("<button>").addClass("btn-success");
       correctChoiceShown();
-      // buttonClear();
+      buttonClear();
       correctAnswers++
+      // $('#results').html('');
 
       if (questionIndex + 1 === charDee.length) {
         winCheck();
@@ -235,7 +237,7 @@ $(document).ready(function() {
           buttonClear();
           showQuestion(questionIndex);
           showPossibleAnswers(questionIndex);
-        }, 5000);
+        }, 2000);
       }
     }
 
@@ -243,6 +245,7 @@ $(document).ready(function() {
       incorrectChoiceShown();
       buttonClear();
       incorrectAnswers++
+      // $('#results').html('');
 
       if (questionIndex + 1 === charDee.length) {
         winCheck();
@@ -254,224 +257,10 @@ $(document).ready(function() {
           buttonClear();
           showQuestion(questionIndex);
           showPossibleAnswers(questionIndex);
-        }, 5000);
+        }, 2000);
       }
     }
 
     questionIndex++;
   });
-
-//     // FUNCTIONS
-//     function startGame() {
-//
-//       console.log("game has started");
-//       // restarts game tallies
-//       currentQuestion = 0;
-//       correctNum = 0;
-//       incorrectNum = 0;
-//       notAnswered = 0;
-//
-//       clearInterval(timerId);
-//
-//       // show game section
-//       $('#game').show();
-//
-//       //  empty last results
-//       $('#results').html('');
-//
-//       // show timer
-//       $('#timerCount').html(timerCount);
-//
-//       // remove start button
-//       $('#start').hide();
-//
-//
-//       // ask first question
-//       nextQuestion();
-//     }
-//
-//     // function that decrements counter and count unanswered if timer runs out
-//     function timerRunning() {
-//
-//       // if timer still has time left and there are still questions left to ask
-//       if(timerCount > -1 && currentQuestion < Object.keys(charDee.questions).length){
-//
-//         $('#timer').text(timerCount);
-//         timerCount--;
-//
-//         if(timerCount === 4) {
-//           $('#timer').addClass('last-seconds');
-//         }
-//       } else if(timerCount === -1) {
-//
-//         notAnswered++;
-//         charDee.result = false;
-//
-//         clearInterval(timerId);
-//         resultId = setTimeout(guessResult, 1000);
-//
-//         $('#results').html('<h3>SHABOOYA! The answer was '+ Object.values(charDee.answers)[charDee.currentQuestion] +'</h3>');
-//
-//       } else if(currentQuestion === Object.keys(charDee.questions).length) {
-//
-//         // adds results of game (correct, incorrect, unanswered) to the page
-//         $('#results')
-//           .html('<h3>Thank you for playing!</h3>'+
-//           '<p>Correct: '+ correctNum +'</p>'+
-//           '<p>Incorrect: '+ incorrectNum +'</p>'+
-//           '<p>Unaswered: '+ notAnswered +'</p>'+
-//           '<p>Please play again!</p>');
-//
-//         // hide game sction
-//         $('#game').hide();
-//
-//         // show start button to begin a new game
-//         $('#start').show();
-//       }
-//     }
-//
-//     startGame();
-//     timerRunning();
-//   });
-//
-//   $(".options").on('click', function() {
-//       guessChecker();
-//   });
-//
-//   // FUNCTIONS
-//   function startGame() {
-//
-//     // restarts game tallies
-//     currentQuestion = 0;
-//     correctNum = 0;
-//     incorrectNum = 0;
-//     notAnswered = 0;
-//
-//     clearInterval(timerId);
-//
-//     // show game section
-//     $('#game').show();
-//
-//     //  empty last results
-//     $('#results').html('');
-//
-//     // show timer
-//     $('#timerCount').html(timerCount);
-//
-//     // remove start button
-//     $('#start').hide();
-//
-//     $('#remaining-time').show();
-//
-//     // ask first question
-//     nextQuestion();
-//   }
-//
-//   function guessChecker() {
-//     // timer ID for gameResult setTimeout
-//     var resultId;
-//
-//     // the answer to the current question being asked
-//     var currentAnswer = Object.values(charDee.answers)[currentQuestion];
-//
-//     // if the text of the option picked matches the answer of the current question, increment correct
-//     if($(this).text() === currentAnswer) {
-//       // turn button green for correct
-//       $(this).addClass('btn-success').removeClass('btn-info');
-//
-//       correctNum++;
-//
-//       clearInterval(timerId);
-//       resultId = setTimeout(guessResult, 1000);
-//       $('#results').html('<h3>Correct Answer!</h3>');
-//
-//     } else {
-//       // turn button clicked red for incorrect
-//       $(this).addClass('btn-danger').removeClass('btn-info');
-//
-//       incorrectNum++;
-//       clearInterval(timerId);
-//       resultId = setTimeout(guessResult, 1000);
-//       $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
-//     }
-//   }
-//
-//   function guessResult() {
-//
-//     // increment to next question set
-//     currentQuestion++;
-//
-//     // remove the options and results
-//     $('.options').remove();
-//     $('#results h3').remove();
-//
-//     // begin next question
-//     nextQuestion();
-//   }
-//
-//   function nextQuestion() {
-//
-//     // set timer to 30 seconds each question
-//     timerCount = 30;
-//     $('#timerCount').removeClass('last-seconds');
-//     $('#timerCount').text(timerCount);
-//
-//     // to prevent timer speed up
-//     if(!timerOn) {
-//       timerId = setInterval(timerRunning, 1000);
-//     }
-//
-//     // gets all the questions then indexes the current questions
-//     var questionContent = Object.values(charDee.questions)[currentQuestion];
-//     $('#question').text(questionContent);
-//
-//     // an array of all the user options for the current question
-//     var questionOptions = Object.values(charDee.answers)[currentQuestion];
-//
-//     // creates all the trivia guess options in the html
-//     $.each(questionOptions, function(index, key) {
-//       $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
-//     })
-//   }
-//
-//   // function that decrements counter and count unanswered if timer runs out
-//   function timerRunning() {
-//
-//     // if timer still has time left and there are still questions left to ask
-//     if(timerCount > -1 && currentQuestion < Object.keys(charDee.questions).length){
-//
-//       $('#timer').text(timerCount);
-//       timerCount--;
-//
-//       if(timerCount === 4) {
-//         $('#timer').addClass('last-seconds');
-//       }
-//     } else if(timerCount === -1) {
-//
-//       notAnswered++;
-//       charDee.result = false;
-//
-//       clearInterval(timerId);
-//       resultId = setTimeout(guessResult, 1000);
-//
-//       $('#results').html('<h3>SHABOOYA! The answer was '+ Object.values(charDee.answers)[charDee.currentQuestion] +'</h3>');
-//
-//     } else if(currentQuestion === Object.keys(charDee.questions).length) {
-//
-//       // adds results of game (correct, incorrect, unanswered) to the page
-//       $('#results')
-//         .html('<h3>Thank you for playing!</h3>'+
-//         '<p>Correct: '+ correctNum +'</p>'+
-//         '<p>Incorrect: '+ incorrectNum +'</p>'+
-//         '<p>Unaswered: '+ notAnswered +'</p>'+
-//         '<p>Please play again!</p>');
-//
-//       // hide game sction
-//       $('#game').hide();
-//
-//       // show start button to begin a new game
-//       $('#start').show();
-//     }
-//   }
-//
 })
